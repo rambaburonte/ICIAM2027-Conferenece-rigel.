ecc10c632f2ad7c75bd4e0161fa0b9ec3638be18
rambaburonte <ronterambabu@gmail.com>
2026-02-06 21:04:49 +0530

    Refactor App component by removing unused imports and cleaning up code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 import Index from './index'
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
 
+function App() {
   return (
     <>
       <Index />
-        
     </>
-  )
+  );
 }
 
-export default App
+export default App;
